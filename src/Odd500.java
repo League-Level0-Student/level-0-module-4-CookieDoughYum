f9582be3920c6fe389042356808338887443005a
CookieDoughYum <league@iMac.attlocal.net>
2018-10-08 16:55:01 -0700

    Finished Module 3 Checkpoint and working on Spud Counter

---
+++ src/Odd500.java
@@ -0,0 +1,13 @@
+
+public class Odd500 {
+public static void main(String[] args) {
+	for(int i=0; i<500; i++) {
+		if(i%2==0) {
+			
+		}
+		else {
+			System.out.println(i);
+		}
+	}
+}
+}
---
+++ src/SpudCounter.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class SpudCounter {
+public static void main(String[] args) {
+	for(int i=1; i<8; i++) {
+		System.out.println(i + " potato" );
+	}
+	System.out.println("more");
+}
+}
